e5452441f3c6f6a19b9892c8ee1c164aabb1b599
Iman Rajabi <iman.rjb@gmail.com>
2022-07-24 16:58:24 +0430

    stable version

--- app/Services/AccessToken/AccessToken.php
+++ app/Services/AccessToken/AccessToken.php
@@ -5,7 +5,12 @@ namespace ImanRjb\JwtAuth\Services\AccessToken;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static getNewAddress($type, $path)
+ * @method static create($user, $userAgent, $ip, $aud = 'web', $scope = '*')
+ * @method static createFromRefreshToken($refreshToken, $userAgent, $ip, $aud = 'web', $scope = '*')
+ * @method static checkToken($token)
+ * @method static revokeByTokenId($tokenId)
+ * @method static revokeByToken($token)
+ * @method static getActiveTokens($user)
  */
 
 class AccessToken extends Facade
