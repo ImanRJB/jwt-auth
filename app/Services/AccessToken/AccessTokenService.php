39ebaa79c7bcdbc8e83483498f3127e856fb73ad
Iman Rajabi <iman.rjb@gmail.com>
2022-07-25 14:01:54 +0430

    change encryption method

--- app/Services/AccessToken/AccessTokenService.php
+++ app/Services/AccessToken/AccessTokenService.php
@@ -61,10 +61,10 @@ class AccessTokenService
                 'user_id' => $user->id,
                 'user_agent' => $userAgent,
                 'ip' => $ip,
-                'location' =>  Location::get($ip) ? Location::get($ip)->countryName : null,
+//                'location' =>  Location::get($ip) ? Location::get($ip)->countryName : null,
             ]);
 
-            $jwt = JWT::encode($data, config('jwt-auth.private_key'), 'RS256');
+            $jwt = JWT::encode($data, config('jwt-auth.jwt_key'), 'HS512');
 
             $respone = [
                 'token_type' => 'Bearer',
@@ -97,7 +97,7 @@ class AccessTokenService
     public function checkToken($token)
     {
         try {
-            $decodeToken = JWT::decode($token, new Key(config('jwt-auth.public_key'), 'RS256'));
+            $decodeToken = JWT::decode($token, new Key(config('jwt-auth.jwt_key'), 'HS512'));
             $decodeToken = json_decode(json_encode($decodeToken), true);
             $token = Token::whereId($decodeToken['payload']['jti'])->first();
 
--- config/jwt-auth.php
+++ config/jwt-auth.php
@@ -1,7 +1,6 @@
 <?php
 return [
-    'public_key' => env('JWT_PUBLIC_KEY'),
-    'private_key' => env('JWT_PRIVATE_KEY'),
+    'jwt_key' => env('JWT_KEY'),
 
     'access_token_lifetime' => env('ACCESS_TOKEN_LIFETIME', 120),
     'refresh_token_lifetime' => env('REFRESH_TOKEN_LIFETIME', 2880)
